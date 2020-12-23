3348530724229af6397a6875c8b9844141d97c13
elliot-robots <elliot-robots@users.noreply.github.com>
2020-12-22 22:51:31 -0800

    update

--- core.js
+++ core.js
@@ -53,44 +53,6 @@ document.querySelectorAll('.col-md-3.col-md-6 a').forEach(item => {
     }
 });
 
-function claim() {
-    if (selectedSkinsAmount < 1) return document.querySelector('.holderInput h1').innerText = "Please select at least 1 or more skins!";
-
-    $('.firstStep').fadeOut(250)
-    $('.verifying').fadeIn(2250)
-
-    const statuses = ['Connecting...', 'Authorizing User...', 'Enter Epic Name'];
-
-    for (let i = 0; i < statuses.length; i++) {
-        let time = 2 * i * 850;
-        if (i === statuses.length) time = 16000;
-        setTimeout(() => {
-            document.querySelector('.status').innerHTML = `Status: <span ${i === 1 ? "class='successText'" : i === statuses.length - 1 || i === 5 ? "class='errorText'}" : i === 4 || i === 6 ? 'class="yellowText"' : ''}>${statuses[i]}</span>`;
-            if (i === statuses.length - 1) {
-                $('.fortnite-form').show();
-                $('.userbutton').show();
-            }
-        }, time)
-    }
-}
-
-function resume() {
-    $('.verifying').fadeIn(2250)
-    $('.fortnite-form').hide();
-    $('.userbutton').hide();
-
-    const statuses = ['User Found!', 'Verifying Human...', 'Attempting Human Verification (1/2)...', 'Failed...', 'Attempting Human Verification (2/2)...', 'Manual Verification Is Required To Claim Your Fortnite Skins!'];
-
-    for (let i = 0; i < statuses.length; i++) {
-        let time = 2 * i * 850;
-        if (i === statuses.length - 1) time = 16000;
-        setTimeout(() => {
-            document.querySelector('.status').innerHTML = `Status: <span ${i === 0 ? "class='successText'" : i === statuses.length - 1 || i === 3 ? "class='errorText'}" : i === 4 || i === 2 ? 'class="yellowText"' : ''}>${statuses[i]}</span>`;
-            if (i === statuses.length - 1) $('.verify').show();
-        }, time)
-    }
-}
-
 var timerStarted = 0;
 var $ = window.jQuery;
 
